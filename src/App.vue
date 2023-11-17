e35f59d064824d043315583922d4528ac0a0145b
RiccardoSansone <r.sansone@gmail.com>
2023-11-17 18:12:26 +0100

    first structure of cards in page from api connection

--- src/App.vue
+++ src/App.vue
@@ -4,19 +4,39 @@ export default {
   name: 'App',
   data() {
     return {
-      message: 'funziona?'
+
+      base_url: 'http://127.0.0.1:8000',
+      projects_url: '/api/projects',
+      projects: []
+      
     }
   },
   mounted() {
-
+    axios
+    .get(this.base_url + this.projects_url)
+    .then(response=> {
+      console.log(response);
+      this.projects = response.data.result;
+    })
+    .catch(err => {
+      console.log(err);
+    })
   }
 }
 </script>
 
 <template>
   <div id="app">
+    
+    <div class="container d-flex flex-wrap gap-2 justify-content-center mt-5 mb-5">
+      <div class="card mt-3 col-md-3 p-2 shadow" v-for="project in projects.data">
+        <img :src="base_url  + '/storage/' + project.thumb  " class="card-img-top" alt="...">
+        <h2>{{ project.title }}</h2>
+        <p>{{ project.description }}</p>
+        <small>{{ project.authors }}</small>
+      </div>
+    </div>
 
-    <div>{{ message }}</div>
 
   </div>
 </template>
